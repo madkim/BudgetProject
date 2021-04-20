645fe76d0fc0e679d96938868dd87e89f92f5d1e
Matthew Kim <madkim@ucsc.edu>
2021-04-19 23:47:41 -0700

    Working on manage budget page.

--- src/_pages/Budget/ManageBudget/index.tsx
+++ src/_pages/Budget/ManageBudget/index.tsx
@@ -30,6 +30,7 @@ import {
   add,
   addOutline,
   settingsOutline,
+  trashBin,
   swapHorizontalOutline,
   trash,
   share,
@@ -57,7 +58,7 @@ const Budget: React.FC<{}> = () => {
           </IonButtons>
 
           <IonTitle className="ion-text-center">
-            <h3>April 2021 Budget</h3>
+            <h4>Manage April Budget</h4>
           </IonTitle>
         </IonToolbar>
       </IonHeader>
@@ -69,100 +70,179 @@ const Budget: React.FC<{}> = () => {
               <IonRow>
                 <IonCol>
                   <IonList>
-                    <IonListHeader lines="inset">
-                      <IonLabel>Income</IonLabel>
-                      <IonNote color="success">
-                        <h2 className="ion-padding-end ">$3500.00</h2>
-                      </IonNote>
-                    </IonListHeader>
-
-                    {/* <div className="ion-padding-start">
-                      <IonItem>
-                        <IonLabel>
-                          <IonRow>
-                            <IonCol>
-                              <h1>Work</h1>
-                            </IonCol>
-                            <IonCol className="ion-text-end">
-                              <h1>$3500.00</h1>
-                            </IonCol>
-                          </IonRow>
-                        </IonLabel>
-                      </IonItem>
-                    </div> */}
+                    <IonItem>
+                      <IonGrid>
+                        <IonRow>
+                          <IonCol size="5">
+                            <h2>Income</h2>
+                          </IonCol>
+                          <IonCol size="5">
+                            <IonNote color="success">
+                              <h2>$3500.00</h2>
+                            </IonNote>
+                          </IonCol>
+                        </IonRow>
+                      </IonGrid>
+                    </IonItem>
                   </IonList>
                 </IonCol>
               </IonRow>
               <IonRow>
                 <IonCol>
                   <IonList>
-                    <IonListHeader lines="inset">
-                      <IonLabel>Expenses</IonLabel>
-                      <IonNote color="danger">
-                        <h2 className="ion-padding-end ">-$79.00</h2>
-                      </IonNote>
-                    </IonListHeader>
+                    <IonItem>
+                      <IonGrid>
+                        <IonRow>
+                          <IonCol size="5">
+                            <h2>Expenses</h2>
+                          </IonCol>
+                          <IonCol size="5" className="ion-padding-start">
+                            <IonNote color="danger">
+                              <h2>$79.00</h2>
+                            </IonNote>
+                          </IonCol>
+                        </IonRow>
+                      </IonGrid>
+                    </IonItem>
 
                     <div className="ion-padding-start">
                       <IonItem>
-                        <IonLabel>
+                        <IonGrid>
                           <IonRow>
-                            <IonCol>
-                              <h1>HBO</h1>
+                            <IonCol size="5">
+                              <h3>
+                                <IonLabel>HBO</IonLabel>
+                              </h3>
                             </IonCol>
-                            <IonCol className="ion-text-end">
-                              <h1>$12.55</h1>
+                            <IonCol size="4" className="ion-text-start">
+                              <h3>
+                                <IonLabel>$12.55</IonLabel>
+                              </h3>
+                            </IonCol>
+                            <IonCol
+                              size="3"
+                              className="ion-text-end ion-no-padding"
+                            >
+                              <IonButton size="default" color="danger">
+                                <IonIcon icon={trashBin} />
+                              </IonButton>
                             </IonCol>
                           </IonRow>
-                        </IonLabel>
+                        </IonGrid>
                       </IonItem>
                       <IonItem>
-                        <IonLabel>
+                        <IonGrid>
                           <IonRow>
-                            <IonCol>
-                              <h1>Netflix</h1>
+                            <IonCol size="5">
+                              <h3>
+                                <IonLabel>Netflix</IonLabel>
+                              </h3>
                             </IonCol>
-                            <IonCol className="ion-text-end">
-                              <h1>$15.00</h1>
+                            <IonCol size="4" className="ion-text-start">
+                              <h3>
+                                <IonLabel>$15.00</IonLabel>
+                              </h3>
+                            </IonCol>
+                            <IonCol
+                              size="3"
+                              className="ion-text-end ion-no-padding"
+                            >
+                              <IonButton size="default" color="danger">
+                                <IonIcon icon={trashBin} />
+                              </IonButton>
                             </IonCol>
                           </IonRow>
-                        </IonLabel>
+                        </IonGrid>
                       </IonItem>
                       <IonItem>
-                        <IonLabel>
+                        <IonGrid>
                           <IonRow>
-                            <IonCol>
-                              <h1>Spotify</h1>
+                            <IonCol size="5">
+                              <h3>
+                                <IonLabel>Spotify</IonLabel>
+                              </h3>
                             </IonCol>
-                            <IonCol className="ion-text-end">
-                              <h1>$9.00</h1>
+                            <IonCol size="4" className="ion-text-start">
+                              <h3>
+                                <IonLabel>$9.00</IonLabel>
+                              </h3>
+                            </IonCol>
+                            <IonCol
+                              size="3"
+                              className="ion-text-end ion-no-padding"
+                            >
+                              <IonButton size="default" color="danger">
+                                <IonIcon icon={trashBin} />
+                              </IonButton>
                             </IonCol>
                           </IonRow>
-                        </IonLabel>
+                        </IonGrid>
                       </IonItem>
                       <IonItem>
-                        <IonLabel>
+                        <IonGrid>
                           <IonRow>
-                            <IonCol>
-                              <h1>AAA</h1>
+                            <IonCol size="5">
+                              <h3>
+                                <IonLabel>AAA</IonLabel>
+                              </h3>
                             </IonCol>
-                            <IonCol className="ion-text-end">
-                              <h1>$35.55</h1>
+                            <IonCol size="4" className="ion-text-start">
+                              <h3>
+                                <IonLabel>$35.55</IonLabel>
+                              </h3>
+                            </IonCol>
+                            <IonCol
+                              size="3"
+                              className="ion-text-end ion-no-padding"
+                            >
+                              <IonButton size="default" color="danger">
+                                <IonIcon icon={trashBin} />
+                              </IonButton>
                             </IonCol>
                           </IonRow>
-                        </IonLabel>
+                        </IonGrid>
                       </IonItem>
                       <IonItem>
-                        <IonLabel>
+                        <IonGrid>
                           <IonRow>
-                            <IonCol>
-                              <h1>Insurance</h1>
+                            <IonCol size="5">
+                              <h3>
+                                <IonLabel>Insurance</IonLabel>
+                              </h3>
                             </IonCol>
-                            <IonCol className="ion-text-end">
-                              <h1>$15.00</h1>
+                            <IonCol size="4" className="ion-text-start">
+                              <h3>
+                                <IonLabel>$15.00</IonLabel>
+                              </h3>
+                            </IonCol>
+                            <IonCol
+                              size="3"
+                              className="ion-text-end ion-no-padding"
+                            >
+                              <IonButton size="default" color="danger">
+                                <IonIcon icon={trashBin} />
+                              </IonButton>
                             </IonCol>
                           </IonRow>
-                        </IonLabel>
+                        </IonGrid>
+                      </IonItem>
+                      <IonItem lines="inset">
+                        <IonGrid>
+                          <IonRow>
+                            <IonCol size="9">
+                              <IonInput placeholder="Add New Expense"></IonInput>
+                            </IonCol>
+
+                            <IonCol
+                              size="3"
+                              className="ion-text-end ion-no-padding"
+                            >
+                              <IonButton size="default" color="success">
+                                <IonIcon icon={addOutline} />
+                              </IonButton>
+                            </IonCol>
+                          </IonRow>
+                        </IonGrid>
                       </IonItem>
                     </div>
                   </IonList>
